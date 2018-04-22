8aa025ccacebd4fd57f1247794721460feab8c0b
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-22 18:23:38 -0300

    Using API Keys with Basic Auth.

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -3,4 +3,24 @@ module.exports = function(server, restify, restifyValidator) {
   server.use(restify.plugins.queryParser());
   server.use(restify.plugins.bodyParser());
   server.use(restifyValidator);
+
+  var restifyPlugins = require('restify').plugins;
+  server.use(restifyPlugins.authorizationParser());
+
+  server.use(function(req, res, next) {
+    var apiKeys = {
+      'user1': 'user1'
+    };
+    if (typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]) {
+      var response = {
+        'status': 'failuse',
+        'data': 'Your must specify a valid API key',
+      };
+      res.setHeader('content-type', 'application/json');
+      res.writeHead(403);
+      res.end(JSON.stringify(response));
+      return response;
+    }
+    return next();
+  });
 }
