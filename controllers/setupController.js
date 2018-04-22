27c38a1075b98f09aa3c14668b4f8f8b7fd9c190
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-22 18:33:57 -0300

    IP Whitelisting.

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -8,6 +8,23 @@ module.exports = function(server, restify, restifyValidator) {
   server.use(restifyPlugins.authorizationParser());
 
   server.use(function(req, res, next) {
+    var whitelistedIps = ['111.222.333.444', '127.0.0.1', '::1'];
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (whitelistedIps.indexOf(ip) === -1) {
+      var response = {
+        'status': 'failuse',
+        'data': 'Invalid IP address',
+        'ip': ip,
+      };
+      res.setHeader('content-type', 'application/json');
+      res.writeHead(403);
+      res.end(JSON.stringify(response));
+      return response;
+    }
+    return next();
+  });
+
+  server.use(function(req, res, next) {
     var apiKeys = {
       'user1': 'user1'
     };
