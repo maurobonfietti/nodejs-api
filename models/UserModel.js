a9b782d591741a628361f3916b7c4692d7e74243
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-04-23 23:19:55 -0300

    Reviewing UserModel.

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+const Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
- id: ObjectId,
- first_name: String,
- last_name: String,
- email: String,
- career: String
+    id: ObjectId,
+    first_name: String,
+    last_name: String,
+    email: String,
+    career: String
 });
 
 const UserModel = mongoose.model('users', UserSchema);
