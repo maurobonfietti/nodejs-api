922a2ea634f52e2b9a46ad5d354301b29ba0ded9
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2019-01-26 16:48:24 -0300

    Move function of delete users, from controller to service.

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,15 +44,6 @@ module.exports = function (server) {
         var errors = req.validationErrors();
         if (errors) return helpers.failure(res, next, errors[0], 400);
 
-        UserModel.findOne({_id: req.params.id}, function (err, user) {
-            if (err) return helpers.failure(res, next, 'Error fetching user from the database', 400);
-            if (user === null) return helpers.failure(res, next, 'The specified user could not be found in the database', 404);
-
-            user.remove(function (err) {
-                if (err) return helpers.failure(res, next, 'Error removing user from to the database', 400);
-
-                helpers.success(res, next, user, 204);
-            });
-        });
+        UserServices.delete(req, res, next);
     });
 };
--- services/userServices.js
+++ services/userServices.js
@@ -70,24 +70,15 @@ module.exports.update = function (req, res, next) {
     });
 };
 
-/*
-module.exports = function (server) {
+module.exports.delete = function (req, res, next) {
+    UserModel.findOne({_id: req.params.id}, function (err, user) {
+        if (err) return helpers.failure(res, next, 'Error fetching user from the database', 400);
+        if (user === null) return helpers.failure(res, next, 'The specified user could not be found in the database', 404);
 
-    server.del("/user/:id", function (req, res, next) {
-        req.assert('id', 'User id is required').notEmpty();
-        var errors = req.validationErrors();
-        if (errors) return helpers.failure(res, next, errors[0], 400);
+        user.remove(function (err) {
+            if (err) return helpers.failure(res, next, 'Error removing user from to the database', 400);
 
-        UserModel.findOne({_id: req.params.id}, function (err, user) {
-            if (err) return helpers.failure(res, next, 'Error fetching user from the database', 400);
-            if (user === null) return helpers.failure(res, next, 'The specified user could not be found in the database', 404);
-
-            user.remove(function (err) {
-                if (err) return helpers.failure(res, next, 'Error removing user from to the database', 400);
-
-                helpers.success(res, next, user, 204);
-            });
+            helpers.success(res, next, user, 204);
         });
     });
 };
-*/
